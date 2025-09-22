90b44b2bfe815c8869d41089ff742aa361a84021
laleexs <alex.sueca.3@gmail.com>
2025-09-22 19:25:12 +0200

    correcion ejercicio1  quitando la id="logo" y cambiando el getElementsById por GetElementsByTagName

--- Archivos AP1/ejercicio1.js
+++ Archivos AP1/ejercicio1.js
@@ -2,6 +2,7 @@
 // Document: representa el documento HTML y permite 
 // acceder a todos los elementos de la página
 
-document.getElementById("logo").src = "src/logo_pepe.png"; // poner comillas dobles
+document.getElementsByTagName("img")[0].src = "src/logo_pepe.png"; // poner comillas dobles
 
 //.src es una propiedad del objeto imagen
+//Para acceder al primer <img>  poner [0]
